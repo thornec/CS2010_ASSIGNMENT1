f0370217dede19a4e92a1d891cff7f4a413e6c36
thornec <thornec@tcd.ie>
2019-02-20 17:23:21 +0000

    Src

--- Sorting/src/SortComparison.java
+++ Sorting/src/SortComparison.java
@@ -1,23 +1,24 @@
 import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
-// -------------------------------------------------------------------------
+class SortComparison {
 
-/**
- *  This class contains static methods that implementing sorting of an array of numbers
- *  using different sort algorithms.
- *
- *  @author
- *  @version HT 2019
- */
-
- class SortComparison {
-
+	public static void main(String[] args) {
+		/*
+    	double[] a = {1,4,0,9,3};
+    	System.out.println(Arrays.toString(a));
+    	mergeSortRecursive(a);
+    	System.out.println(Arrays.toString(a) + " ! ");
+    	*/
+	}
+	
     /**
      * Sorts an array of doubles using InsertionSort.
      * This method is static, thus it can be called as SortComparison.sort(a)
      * @param a: An unsorted array of doubles.
      * @return array sorted in ascending order.
-     *
+     *	
      */
     static double [] insertionSort (double a[]){
     	double temp;
@@ -34,7 +35,7 @@ import java.util.Arrays;
     	}
     	return a;
     }
-
+    
     /**
      * Sorts an array of doubles using Quick Sort.
      * This method is static, thus it can be called as SortComparison.sort(a)
@@ -43,11 +44,63 @@ import java.util.Arrays;
      *
      */
     static double [] quickSort (double a[]){
-	
-		 //todo: implement the sort
-
-    }//end quicksort
-
+    	shuffleArray(a);
+    	sortQuick(a, 0, a.length-1);
+		return a;
+    }
+    
+    /*Recursive method used for quicksort*/
+    private static void sortQuick(double[] a, int lo, int hi){
+    	if(hi <= lo) return; //already sorted
+    	int j = partition(a, lo, hi);
+    	sortQuick(a, lo, j-1);
+    	sortQuick(a, j+1, hi);
+    } 
+    
+    /*Method used for partitioning Array*/
+    private static int partition(double a[], int lo, int hi){
+    	int i = lo;
+    	int j = hi+1;
+    	while(true){
+    		/* Find item on left to swap */
+    		while((a[++i] < a[lo])){
+    			if(i ==hi) break;
+    		}
+    		/* Find items on right to swap */
+    		while((a[lo] < a[--j])){
+    			if(j == lo) break;
+    		}
+    		/* Check if pointers j and i cross */
+    		if(i >= j) break;
+    		exchange(a, i, j);	//swap
+    	}
+    	exchange(a, lo, j);	//swap with pivot
+    	return j;	//return index of item known to be in place
+    }
+    
+    /* Method used to swap to elements of array */
+    private static void exchange(double a[], int i, int j){
+    	if(a.length <= 0)return;
+    	
+    	double temp = a[i];
+    	a[i] = a[j];
+    	a[j] = temp;
+    }
+    
+    /* Implementing Fisher–Yates shuffle */
+    static void shuffleArray(double[] ar)
+    {
+      Random rnd = ThreadLocalRandom.current();
+      for (int i = ar.length - 1; i > 0; i--)
+      {
+        int index = rnd.nextInt(i + 1);
+        // Simple swap
+        double a = ar[index];
+        ar[index] = ar[i];
+        ar[i] = a;
+      }
+    }
+    
     /**
      * Sorts an array of doubles using Merge Sort.
      * This method is static, thus it can be called as SortComparison.sort(a)
@@ -62,14 +115,17 @@ import java.util.Arrays;
      * @param a: An unsorted array of doubles.
      * @return after the method returns, the array must be in ascending sorted order.
      */
-
+    
     static double[] mergeSortIterative (double a[]) {
-
-		 //todo: implement the sort
-	
-    }//end mergesortIterative
-    
-    
+    	int N = a.length;
+    	double[] aux = new double[a.length];
+    	for(int size = 1; size < N; size = size+size){
+    		for(int lo =0; lo < N-size; lo+= size+size){
+    			merge(a, aux,lo, lo+size-1, Math.min(lo+size+size-1, N-1));
+    		}
+    	}
+    	return a;
+    }
     
     /**
      * Sorts an array of doubles using recursive implementation of Merge Sort.
@@ -78,13 +134,59 @@ import java.util.Arrays;
      * @param a: An unsorted array of doubles.
      * @return after the method returns, the array must be in ascending sorted order.
      */
+    
     static double[] mergeSortRecursive (double a[]) {
+    	int N = a.length;
+    	double[] aux = new double[N];
+    	sort(a, aux, 0, N-1);
+    	return a;
+   }
+    /*Lo - First Part of Array to be sorted, Hi is Second Part of Array to be sorted and
+     * Mid is the midpoint of the array */  
+    private static void merge(double[] a, double[] aux, int lo, int mid, int hi){
+    	assert isSorted(a);	//precondition: a[lo..mid] sorted
+    	assert isSorted(a);	//precondition: a[mid+1..hi] sorted
     	
-
-    	//todo: implement the sort
-	
-   }//end mergeSortRecursive
+    	/* Copy contents of array into auxillery array */
+    	for(int k = lo; k <= hi; k++){
+    		aux[k] = a[k];
+    	}
+    	/* Merge both arrays into one */
+    	int i = lo; //start of lo
+    	int j = mid+1; //start of hi
     	
+    	for(int k = lo; k <= hi; k++){
+    		if(i > mid){ a[k] = aux[j++];}
+    		else if(j > hi){ a[k] = aux[i++];}
+    		else if((aux[j] < aux[i])){ a[k] = aux[j++];}
+    		else{ a[k] = aux[i++];}
+    	}
+    	assert isSorted(a);
+    }
+    /* Recursive Method used to create sub arrays, sort them and then merge them  */
+    private static void sort(double[] a, double[] aux, int lo, int hi){
+    	int CUTOFF = 7;
+    	if(hi <= lo + CUTOFF -1){
+    		insertionSort(a);
+    		return;
+    	}
+    	if(hi <= lo)return; //check we have something to do
+    	int mid = lo + (hi - lo) / 2; //compute midpoint
+    	sort(a, aux, lo, mid); //sort first half
+    	sort(a, aux, mid+1, hi);//sort second half
+    	if(!(a[mid+1] < a[mid])) return; //already sorted
+    	merge(a, aux, lo, mid, hi);//merge both halves
+    }
+    
+    /*Checks if array is sorted */
+    private static boolean isSorted(double[] a){
+    	boolean sorted = true;
+    	
+    	for(int i = 0; i < a.length-1; i++){
+    		if(a[i] > a[i+1]) sorted =  false;
+    	}
+    	return sorted;
+    }
     
     /**
      * Sorts an array of doubles using Selection Sort.
@@ -93,21 +195,27 @@ import java.util.Arrays;
      * @return array sorted in ascending order
      *
      */
+    
     static double [] selectionSort (double a[]){
-
-         //todo: implement the sort
-
-    }//end selectionsort
-
-   
-
-    public static void main(String[] args) {
-    	double[] a = {1,4,0,9,3};
-    	System.out.println(Arrays.toString(a));
-    	insertionSort(a);
-    	System.out.println(Arrays.toString(a) + " ! ");
-
-        //todo: do experiments as per assignment instructions
+        int arrayLength = a.length;
+        
+        for (int unsortIndex = arrayLength - 1; unsortIndex > 0; unsortIndex--) {
+            int largest = 0;
+            for (int i = 1; i <= unsortIndex; i++) {
+                if (a[i] > a[largest]) {
+                    largest = i;
+                }
+            }
+            swap(a, largest, unsortIndex);
+        }
+        return a;
     }
+    
+    /* Method used to swap to elements of an array */
+    private static void swap(double[] a, int x, int y) {
+        double temp = a[x];
+        a[x] = a[y];
+        a[y] = temp;
+    } 
 
- }//end class
+}
